dc54184b40b08c97d8b05db75e03b881c0a560e8
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-12-02 19:12:16 +0900

    feat: Add Env Settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    HealthModule,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
