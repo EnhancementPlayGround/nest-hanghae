a17c12534a18b1cb54a509d99fe11828d7f333a3
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2024-01-06 12:14:56 +0900

    Feat: Query Log Setting

--- src/core/DatabaseClient.ts
+++ src/core/DatabaseClient.ts
@@ -1,10 +1,28 @@
 import { OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
+import Logger from './logger/Logger';
 
 export default class DatabaseClient
-  extends PrismaClient
+  extends PrismaClient<Prisma.PrismaClientOptions, 'query'>
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor(private readonly logger: Logger) {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+      ],
+    });
+    
+    this.$on('query', (e) =>
+      logger.info(
+        `Query: ${e.query}, Params: ${e.params}, Duration: ${e.duration}ms`,
+      ),
+    );
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
--- src/infra/apis/filter/LoggingExceptionFilter.ts
+++ src/infra/apis/filter/LoggingExceptionFilter.ts
@@ -1,36 +1,37 @@
 import {
-    ExceptionFilter,
-    Catch,
-    ArgumentsHost,
-    HttpException,
-    HttpStatus,
-    Logger
-  } from '@nestjs/common';
-  
-  @Catch()
-  export class LoggingExceptionFilter implements ExceptionFilter {
-    private readonly logger = new Logger(LoggingExceptionFilter.name);
-  
-    catch(exception: unknown, host: ArgumentsHost) {
-      const ctx = host.switchToHttp();
-      const response = ctx.getResponse();
-      const request = ctx.getRequest();
-      const status = exception instanceof HttpException
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+
+@Catch()
+export class LoggingExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(LoggingExceptionFilter.name);
+
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+    const status =
+      exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
-  
-      const logFormat = `Request URL: ${request.url}, Method: ${request.method}, Status Code: ${status}, Error: ${exception}`;
-  
-      this.logger.error(logFormat);
-  
-      response
-        .status(status)
-        .json({
-          statusCode: status,
-          timestamp: new Date().toISOString(),
-          path: request.url,
-          error: exception instanceof HttpException ? exception.getResponse() : 'Internal Server Error'
-        });
-    }
+
+    const logFormat = `Request URL: ${request.url}, Method: ${request.method}, Status Code: ${status}, Error: ${exception}`;
+
+    this.logger.error(logFormat);
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      error:
+        exception instanceof HttpException
+          ? exception.getResponse()
+          : 'Internal Server Error',
+    });
   }
-  
+}
